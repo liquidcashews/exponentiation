51359e32eec087270f64e45291f70c2ec1d0d694
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2025-11-29 07:20:23 +0000

    expantanum

--- js/layers.js
+++ js/layers.js
@@ -27,7 +27,7 @@ addLayer("p", {
     upgrades: {
         11: {description: "Point generation is faster based on your things.",
                 cost: new ExpantaNum(1),
-                unlocked() { return (hasUpgrade(this.layer, 11))},
+                unlocked() {return true},
                 effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
                     let ret = player[this.layer].points.add(1).pow(1.1)
                     if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
